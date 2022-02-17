40562cd178b256fd4858ffdc59f2487819a68709
FFTeixeira <fernandoft76@hotmail.com>
2022-02-17 01:08:09 -0300

    Testando primeira classe de domínio - Categoria

--- src/main/java/com/fftxr/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fftxr/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.fftxr.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fftxr.cursomc.entities.Categoria;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String findAll() {
+	public List<Categoria> findAll() {
 		
-		return "REST funcionando!";
+		Categoria cat1 = new Categoria(1L, "Informática");
+		Categoria cat2 = new Categoria(2L, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 }
